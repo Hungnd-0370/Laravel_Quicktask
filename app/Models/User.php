9ae54ecfff9930532f1cefe7a2dd79dba05a146d
Hungnd-0370 <nguyen.duy.hung-b@sun-asterisk.com>
2023-08-29 08:30:48 +0700

    Chapter 04 : Accessors, mutators, Scope

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Str;
 
 class User extends Authenticatable
 {
@@ -51,5 +55,30 @@ class User extends Authenticatable
     {
         return $this->BelongsToMany(Role::class)
             ->withTimestamps();
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->attributes['first_name'] . ' ' .  $this->attributes['last_name'],
+        );
+    }
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Str::slug($value),
+        );
+    }
+
+    public function scopeAdmin(Builder $query): void
+    {
+        $query->where('is_admin', '=', true);
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('activeScope', function (Builder $builder) {
+            $builder->where('is_active', '=', true);
+        });
     }
 }
